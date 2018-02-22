86ac340a5604411e5d1296186eaae22bccde26e5
John Steven <houtaro12@gmail.com>
2018-02-23 04:16:09 +0800

    Committed: Signin and Register activities. Pag register lng ug account then signin. Giconnect nako na sa GMAP Activity

---
+++ app/src/main/java/com/example/keima/ambulife/RegisterScreen.java
@@ -0,0 +1,201 @@
+package com.example.keima.ambulife;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class RegisterScreen extends AppCompatActivity{
+
+    EditText    editText_regFirstname, editText_regLastname,
+                editText_regUsername, editText_regPassword,
+                editText_regCPassword;
+    RadioButton radioBtnTerms;
+    Button      btnRegister, btnGotoSignin;
+    ProgressBar progressBar;
+
+    // Declare Firebase object
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+
+    // Declare Firebase User Information
+//    String userEmail;
+
+    public void onStart() {
+        super.onStart();
+        // Check if the user is currently signed in or not null. Then update UI
+        FirebaseUser tmp = mAuth.getCurrentUser();
+        currentUser = tmp;
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+
+        // Instantiate Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        editText_regFirstname = (EditText) findViewById(R.id.registerFirstName);
+        editText_regLastname = (EditText) findViewById(R.id.registerLastName);
+        editText_regUsername = (EditText) findViewById(R.id.registerUsername);
+        editText_regPassword = (EditText) findViewById(R.id.registerPassword);
+        editText_regCPassword = (EditText) findViewById(R.id.registerCPassword);
+        radioBtnTerms = (RadioButton) findViewById(R.id.registerTermsRadioBtn);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnGotoSignin = (Button) findViewById(R.id.btnBackSignin);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
+        btnGotoSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterScreen.this, SigninScreen.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    // Function to toggle registration form enable(VISIBLE)/disable(GONE)
+    public void toggleRegistrationFields(Integer visibility){
+        editText_regFirstname.setVisibility(visibility);
+        editText_regLastname.setVisibility(visibility);
+        editText_regUsername.setVisibility(visibility);
+        editText_regPassword.setVisibility(visibility);
+        editText_regCPassword.setVisibility(visibility);
+        radioBtnTerms.setVisibility(visibility);
+        btnRegister.setVisibility(visibility);
+    }
+
+    public void registerUser() {
+        String firstname = editText_regFirstname.getText().toString().trim();
+        String lastname = editText_regLastname.getText().toString().trim();
+        String username = editText_regUsername.getText().toString().trim();
+        String password = editText_regPassword.getText().toString().trim();
+        String confirm_password = editText_regCPassword.getText().toString().trim();
+
+        // Check to make sure all the fields are filled-in
+        if (firstname.isEmpty()) {
+            editText_regFirstname.setError("Please enter your First Name");
+            editText_regFirstname.requestFocus();
+            return;
+        }
+        if (lastname.isEmpty()) {
+            editText_regLastname.setError("Please enter your Last Name");
+            editText_regLastname.requestFocus();
+            return;
+        }
+        if (username.isEmpty()) { // Email should be Email
+            editText_regUsername.setError("Please enter your username (Email Required)");
+            editText_regUsername.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(username).matches()) {
+            editText_regUsername.setError("Please enter a valid email");
+            editText_regUsername.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editText_regPassword.setError("Please enter your password");
+            editText_regPassword.requestFocus();
+            return;
+        }
+        if (confirm_password.isEmpty()) {
+            editText_regCPassword.setError("Please re-enter your password");
+            editText_regCPassword.requestFocus();
+            return;
+        }
+        if (!password.equals(confirm_password)) {
+            Toast.makeText(this.getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (password.length() < 6) {
+            editText_regPassword.setError("Minimum length of 6 characters");
+            return;
+        }
+        if (!radioBtnTerms.isChecked()) {
+            Toast.makeText(this.getApplicationContext(), "You have to agree to the terms and conditions to proceed", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Show progress bar
+        toggleRegistrationFields(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+
+
+        // Sign up the new users
+        mAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // When task is complete
+                if (task.isSuccessful()) {
+                    // If the sign in is successful, display a message to the user
+                    Toast.makeText(getApplicationContext(), "Registration Successful",
+                            Toast.LENGTH_SHORT).show();
+                    updateDisplayName();
+                    Intent intent = new Intent(RegisterScreen.this, SigninScreen.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // If sign in fails, display a message to the user.
+                    progressBar.setVisibility(View.GONE);
+                    toggleRegistrationFields(View.VISIBLE);
+                    Toast.makeText(getApplicationContext(), "Registration Failed",
+                            Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Email already been used",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    // Update the profile of the user
+    public void updateDisplayName(){
+        // Declare and Initialize the user's first name and last name
+        String fname = editText_regFirstname.getText().toString().trim();
+        String lname = editText_regLastname.getText().toString().trim();
+        final String displayName = fname+" "+lname; // Full name will be the display name
+
+        final FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null){
+            UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(displayName).build();
+
+            user.updateProfile(profileUpdate)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "Welcome, "+
+                                        user.getDisplayName(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/keima/ambulife/SigninScreen.java
@@ -0,0 +1,194 @@
+package com.example.keima.ambulife;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+public class SigninScreen extends AppCompatActivity {
+
+    // Declare Firebase object
+    private FirebaseAuth mAuth;
+//    private FirebaseUser currentUser;
+
+    // Declare Views here
+    TextView signinText, signinText2;
+    EditText signin_username, signin_password;
+    Button gotoRegister, btnSignin;
+    ProgressBar progressBar;
+
+
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_screen);
+
+        // Instantiate Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Assign views by id
+        signinText = (TextView) findViewById(R.id.signinText);
+        signinText2 = (TextView) findViewById(R.id.signinText2);
+        signin_username = (EditText) findViewById(R.id.signinUsername);
+        signin_password = (EditText) findViewById(R.id.signinPassword);
+        btnSignin = (Button) findViewById(R.id.btnSignin);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        gotoRegister = (Button) findViewById(R.id.btnBackToRegister);
+
+        gotoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SigninScreen.this, RegisterScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInUser();
+            }
+        });
+
+    }
+
+    // Check if there is a user currently signed in
+    // If there is a current user signed in, then the user must log out first
+    public void onStart() {
+        super.onStart();
+        // Check if the user is currently signed in or not null. Then update UI
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user!= null){
+            userExists(user);
+        }
+    }
+
+
+    // If user signed in, change UI
+    public void userExists(FirebaseUser user){
+        signin_username.setVisibility(GONE);
+        signin_password.setVisibility(GONE);
+
+        signinText.setText("Welcome, "+user.getDisplayName());
+        signinText2.setVisibility(View.VISIBLE);
+        gotoRegister.setText("Sign out");
+
+        // btnSignin will now automatically sign in user
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SigninScreen.this, MapsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        // gotoRegister will now become sign out button
+        gotoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOutUser();
+            }
+        });
+
+    }
+
+
+    // Sign out user
+    public void signOutUser(){
+        // Hide Buttons
+        progressBar.setVisibility(View.VISIBLE);
+        toggleSignInFields(View.GONE);
+        // Sign the user out
+        mAuth.signOut();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                refreshActivity();
+                Toast.makeText(getApplicationContext(), "You've signed out",
+                        Toast.LENGTH_LONG).show();
+            }
+        }, 2000);
+    }
+
+
+    // Refresh the current activity
+    public void refreshActivity(){
+        finish();
+        startActivity(getIntent());
+    }
+
+
+    // Function to toggle registration form enable(VISIBLE)/disable(GONE)
+    public void toggleSignInFields(Integer visibility){
+        signin_username.setVisibility(visibility);
+        signin_password.setVisibility(visibility);
+        btnSignin.setVisibility(visibility);
+        gotoRegister.setVisibility(visibility);
+    }
+
+    public void signInUser(){
+        String email = signin_username.getText().toString().trim();
+        String password = signin_password.getText().toString().trim();
+
+        // Check to make sure the fields are not empty
+        if (email.isEmpty()) {
+            signin_username.setError("Please enter your username");
+            signin_username.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            signin_password.setError("Please enter your password");
+            signin_password.requestFocus();
+            return;
+        }
+
+        // Show Progress bar disable fields
+        toggleSignInFields(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+
+        // Sign in user
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    progressBar.setVisibility(View.GONE);
+                    toggleSignInFields(View.VISIBLE);
+                    Toast.makeText(getApplicationContext(), "Signed in successfully", Toast.LENGTH_LONG).show();
+                    refreshActivity();
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(getApplicationContext(), "Sign in failed", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                    toggleSignInFields(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+
+
+}
