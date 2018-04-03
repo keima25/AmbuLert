4f8e2013a4dfc6d2a9a9f01f4bccab5858238abe
John Steven <houtaro12@gmail.com>
2018-04-04 01:40:34 +0800

    Minor changes on UI

--- app/src/main/java/com/example/keima/ambulife/ProfileFragment.java
+++ app/src/main/java/com/example/keima/ambulife/ProfileFragment.java
@@ -1,11 +1,15 @@
 package com.example.keima.ambulife;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by Steven on 02/04/2018.
@@ -13,10 +17,38 @@ import android.view.ViewGroup;
 
 public class ProfileFragment extends Fragment {
 
+    ImageView closeBtn;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, null);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        closeBtn = (ImageView) view.findViewById(R.id.closeButton);
+
+
+        //When close button is clicked
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Change back to MapsActivity Fragment
+                Fragment fragment = null;
+                fragment = new MapsActivity();
+                if(fragment != null){
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction ft = fragmentManager.beginTransaction();
+
+                    ft.replace(R.id.screen_area, fragment);
+                    ft.commit();
+                }
+
+            }
+        });
+    }
 }
