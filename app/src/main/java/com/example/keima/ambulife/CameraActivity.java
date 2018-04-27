cb2b310ad897e344bd9ce9856f0ae66c73cc1bb8
Keima <katsuragikeima25@yahoo.com>
2018-04-27 18:45:05 +0800

    CameraActivity

--- app/src/main/java/com/example/keima/ambulife/CameraActivity.java
+++ app/src/main/java/com/example/keima/ambulife/CameraActivity.java
@@ -1,50 +1,132 @@
 package com.example.keima.ambulife;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.Manifest;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.karan.churi.PermissionManager.PermissionManager;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import permissions.dispatcher.OnShowRationale;
 import permissions.dispatcher.PermissionRequest;
 import permissions.dispatcher.RuntimePermissions;
 
-/**
- * Created by Steven on 13/04/2018.
- */
 
 public class CameraActivity extends AppCompatActivity {
 
-    static final int REQUEST_TAKE_PHOTO = 0;
-
     ImageView cameraView;
     Button cameraButton;
+    ProgressDialog progress;
+    private static final int CAMERA_REQUEST_CODE = 1;
+    private StorageReference Storage;
+    PermissionManager permissionManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_camera);
 
-        initViews();
+        cameraButton = findViewById(R.id.cameraBtn);
+        cameraView = findViewById(R.id.cameraView);
+        progress = new ProgressDialog(this);
+        Storage = FirebaseStorage.getInstance().getReference();
+
+        permissionManager = new PermissionManager() {};
+        permissionManager.checkAndRequestPermissions(this);
+
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
 
     }
 
-    private void initViews() {
-        cameraView = (ImageView) findViewById(R.id.cameraView);
-        cameraButton = (Button) findViewById(R.id.cameraBtn);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionManager.checkResult(requestCode, permissions, grantResults);
+
+        ArrayList<String> granted = permissionManager.getStatus().get(0).granted;
+        ArrayList<String> denied = permissionManager.getStatus().get(0).denied;
+
+        for (String item : granted)
+            Log.e("Permission Granted: ", item);
+
+        for (String item : denied)
+            Log.e("Permission Denied: ", item);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
+            progress.setMessage("Uploading");
+            progress.show();
+
+            //get camera image
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] dataBAOS = baos.toByteArray();
+            //set the image
+            cameraView.setImageBitmap(bitmap);
+            //upload Pic to FireBase
+            StorageReference Storage = FirebaseStorage.getInstance().getReferenceFromUrl("gs://ambulert-c1124.appspot.com/Photos");
+            StorageReference imagesRef = Storage.child(""+ new Date().getTime());
+            //uploading the image
+            UploadTask uploadTask = imagesRef.putBytes(dataBAOS);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(),"Sending failed", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    progress.dismiss();
+                }
+            });
+        }
+    }
 }
